030d2b8cfba9a166db63566fd191e0406ff4caaa
DHG <duyhung8a2@gmail.com>
2023-05-04 19:58:20 +0700

    basic css

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
-
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import Layout, { siteTitle } from '@/components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <main>
-      <Link href="/posts/first-post"> This is first post</Link>
-    </main>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello Motherfacker</p>
+      </section>
+    </Layout>
   )
 }
--- frontend/src/pages/posts/first-post.tsx
+++ frontend/src/pages/posts/first-post.tsx
@@ -1,11 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '@/components/layout';
+
 export default function FirstPost() {
     return (
-        <>
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">Back to Home</Link>;
-        </h2>
-        </>
+        <Layout home>
+            <Head>
+                <title>First post</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            
+
+            <div className='w-1/2 mx-auto mt-6 items-center justify-center  bg-white rounded-lg border-2 border-blue-200  shadow-lg cursor-pointer transition transform-all duration-200 hover:scale-105 hover:border-blue-400 '>
+                <h1 className='text-3xl py-4 font-extrabold text-center'>First Post</h1>
+            </div>
+
+            <h2>
+                <Link href="/">Back to Home</Link>;
+            </h2>
+        </Layout>
     );
 }
